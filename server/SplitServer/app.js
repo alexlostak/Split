aa496d2f3f0145140d756664c4bc5862e9b639ac
AlejandroStevensonDuran <astevensond@utexas.edu>
2018-02-13 21:08:24 -0600

    faulty version

--- server/SplitServer/app.js
+++ server/SplitServer/app.js
@@ -1,14 +1,77 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var server = require('http').Server(app);
 var app = express();
+
+var braintree = require("braintree");
+
+
+//braintree
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "7yscfj8n3rd947jj",
+  publicKey: "7yscfj8n3rd947jj",
+  privateKey: "4d11fbce8b80fdf9f3d9b55370247884"
+});
+// generate student token
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+
+/* process a sale(checkout) */
+app.post("/checkout", function (req, res) {
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  var amount = req.body.amount;
+  console.log("amount is: " + amount);
+  // Make sale
+  var result = gateway.transaction.sale({
+  amount: amount,
+  paymentMethodNonce: nonceFromTheClient,
+  options: {
+    submitForSettlement: true
+  }
+  }, function (err, result) {
+      if (result) {
+        res.send(result);
+  console.log("transaction succeeded");
+      } else {
+  console.log("transaction failed");
+        res.status(500).send(error);
+      }
+  });
+});
+
+
+/* Set up Server and Routes */
+app.get('/',function(req, res) {
+  res.sendFile(path.join(__dirname,"index.html"));
+});
+
+//for client
+app.use('/client', express.static('client'));
+var port = process.env.PORT || 80;  //tries to fetch port from env. variables, otherwise 80
+server.listen(port); //Start server on port
+console.log("Server started.");
+
+
+//braintree token the hard one TODO
+// gateway.clientToken.generate({
+//   customerId: aCustomerId
+// }, function (err, response) {
+//   var clientToken = response.clientToken
+// });
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +79,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -26,11 +89,12 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -42,5 +106,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
