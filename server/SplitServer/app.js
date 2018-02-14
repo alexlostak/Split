d3c7b92e3f874aadc8e1626932cde48f44cea629
gilad croll <giladcroll@utexas.edu>
2018-02-13 21:31:50 -0600

    half fix braintree

--- server/SplitServer/app.js
+++ server/SplitServer/app.js
@@ -1,17 +1,21 @@
-var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var server = require('http').Server(app);
-var app = express();
+/*
+ * Server Sided NodeJS
+ */
 
-var braintree = require("braintree");
+ /* Dependencies */
+ var express = require('express');
+ var path = require('path');
+ var app = express();
+ var serv = require('http').Server(app);
+ var bodyParser = require('body-parser');
+ var braintree = require("braintree");
+ var cookieParser = require('cookie-parser'); 
+ 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //braintree
 var gateway = braintree.connect({
@@ -20,7 +24,18 @@ var gateway = braintree.connect({
   publicKey: "7yscfj8n3rd947jj",
   privateKey: "4d11fbce8b80fdf9f3d9b55370247884"
 });
-// generate student token
+
+/* Set up Server and Routes */
+app.get('/',function(req, res) {
+  res.sendFile(path.join(__dirname,"index.html"));
+});
+app.use('/client', express.static('client'));
+var port = process.env.PORT || 80;  //tries to fetch port from env. variables, otherwise 80
+serv.listen(port); //Start server on port
+console.log("Server started.");
+
+/*** braintree related interaction  ***/
+/* provide client with token */
 app.get("/client_token", function (req, res) {
   gateway.clientToken.generate({}, function (err, response) {
     res.send(response.clientToken);
@@ -45,67 +60,7 @@ app.post("/checkout", function (req, res) {
   console.log("transaction succeeded");
       } else {
   console.log("transaction failed");
-        res.status(500).send(error);
+        res.status(500).send(err);
       }
   });
 });
-
-
-/* Set up Server and Routes */
-app.get('/',function(req, res) {
-  res.sendFile(path.join(__dirname,"index.html"));
-});
-
-//for client
-app.use('/client', express.static('client'));
-var port = process.env.PORT || 80;  //tries to fetch port from env. variables, otherwise 80
-server.listen(port); //Start server on port
-console.log("Server started.");
-
-
-//braintree token the hard one TODO
-// gateway.clientToken.generate({
-//   customerId: aCustomerId
-// }, function (err, response) {
-//   var clientToken = response.clientToken
-// });
-
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-module.exports = app;
